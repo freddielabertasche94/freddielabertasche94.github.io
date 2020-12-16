3e654d96ad5f88bf533cad23caea28ad8ae47c2b
defaultuse0 <65107722+defaultuse0@users.noreply.github.com>
2020-12-16 20:12:36 +0100

    Add files via upload

---
+++ wunschliste/script.js
@@ -0,0 +1,9 @@
+var username = sign = window.prompt('Benutzername');
+var password = sign = window.prompt('Passwort');
+while (username != 'admin') {
+
+}
+while (password != 'admin') {
+  alert('Nein');
+  window.replace(https://freddielabertasche94.github.io/wunschliste/nope.html);
+}
