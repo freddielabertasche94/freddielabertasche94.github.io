db4af99cc31e7261bd69d0b8698db1afcbd2f845
defaultuse0 <65107722+defaultuse0@users.noreply.github.com>
2020-12-16 20:25:52 +0100

    Delete script.js

--- wunschliste/assets/script.js
+++
@@ -1,9 +0,0 @@
-var username = sign = window.prompt('Benutzername');
-var password = sign = window.prompt('Passwort');
-while (username != 'admin') {
-  alert('Seite neuladen');
-}
-while (password != 'admin') {
-  alert('Seite neuladen');
-
-}
