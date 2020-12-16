e04d3b04cc03270f4d397614c1af21018a23c83c
defaultuse0 <65107722+defaultuse0@users.noreply.github.com>
2020-12-16 20:23:41 +0100

    Create script.js

---
+++ wunschliste/assets/script.js
@@ -0,0 +1,9 @@
+var username = sign = window.prompt('Benutzername');
+var password = sign = window.prompt('Passwort');
+while (username != 'admin') {
+  alert('Seite neuladen');
+}
+while (password != 'admin') {
+  alert('Seite neuladen');
+
+}
